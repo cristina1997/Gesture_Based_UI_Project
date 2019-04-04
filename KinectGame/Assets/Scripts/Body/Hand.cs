0d29e2176129e141ae7e79ca95b383cf4b6cd471
unknown <stoyanrizov6@gmail.com>
2019-04-04 12:54:28 +0100

    KinectV2 to KinectV1 character creation

---
+++ KinectGame/Assets/Scripts/Body/Hand.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public Transform mHandMesh;
+    public static bool isDuckDestroyed;
+    public int scoreValue;
+    private DuckManager duckManager;
+
+
+    private void Start()
+    {
+         GameObject duckManagerObject = GameObject.Find("DuckManager");
+        if (duckManagerObject != null)
+        {
+            duckManager = duckManagerObject.GetComponent <DuckManager>();
+            scoreValue = 1;
+        }
+        if (duckManager == null)
+        {
+            Debug.Log ("Cannot find 'GameController' script");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // we set he hand mesh posittion to follow the joint
+        mHandMesh.position = Vector3.Lerp(mHandMesh.position, transform.position, Time.deltaTime * 15.0f);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if the object is collected then give the boolean a value of true
+        if (!collision.gameObject.CompareTag("Duck"))
+            return;
+        else if (collision.gameObject.CompareTag("Duck"))
+        {
+            duckManager.AddScore(scoreValue);
+            isDuckDestroyed = true;
+        }
+
+        Duck duck = collision.gameObject.GetComponent<Duck>();
+        StartCoroutine(duck.DestroyDucks());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isDuckDestroyed = false;
+    }
+
+}
---
+++ KinectGame/Assets/Scripts/Collectible/DuckManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DuckManager : MonoBehaviour
+{
+    public GameObject duckPrefab;
+
+    private List<Duck> allDucks = new List<Duck>();
+    private Vector2 bottomLeft = Vector2.zero;
+    private Vector2 topRight = Vector2.zero;
+    //private new Camera camera;
+
+    public Text scoreText;
+    private int score;
+
+    private void Awake()
+    {
+
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.farClipPlane));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector3(
+            Camera.main.pixelWidth, 
+            Camera.main.pixelHeight / 2, 
+            Camera.main.farClipPlane));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //score = 0;
+        //UpdateScore();
+        StartCoroutine(CreateDucks());
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.farClipPlane)), 0.5f);
+        Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(new Vector3(
+            Camera.main.pixelWidth, 
+            Camera.main.pixelHeight, 
+            Camera.main.farClipPlane)), 0.5f);
+       
+    }
+
+    //private void Spawn()
+    //{
+    //    float distanceFromCamera = camera.nearClipPlane; // Change this value if you want
+    //    Vector3 topLeft = camera.ViewportToWorldPoint(new Vector3(0, 1, distanceFromCamera));
+    //    Vector3 topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, distanceFromCamera));
+    //    Vector3 spawnPoint = Vector3.Lerp(topLeft, topRight, Random.value); // Get a random point between the topLeft and topRight point
+    //    GameObject clone = Instantiate(prefab, spawnPoint, Quaternion.identity);
+    //}
+
+    public Vector3 GetPlanePosition()
+    {
+
+        //float distanceFromCamera = camera.nearClipPlane; // Change this value if you want
+        //Vector3 topLeft = camera.ViewportToWorldPoint(new Vector3(0, 1, distanceFromCamera));
+        //Vector3 topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, distanceFromCamera));
+        //Vector3 spawnPoint = Vector3.Lerp(topLeft, topRight, Random.value); // Get a random point between the topLeft and topRight point
+
+        // Random position
+        float targetX = Random.Range(bottomLeft.x, topRight.x);
+        float targetY = Random.Range(bottomLeft.y, topRight.y);
+
+        return new Vector3(targetX, targetY, 0);
+    }
+
+    private IEnumerator CreateDucks()
+    {
+        while (allDucks.Count < 5)
+        {
+
+            // Create and add the ducks
+            GameObject newDuckObj = Instantiate(duckPrefab, GetPlanePosition(), Quaternion.identity, transform);
+            Duck newDuck = newDuckObj.GetComponent<Duck>();
+
+            // Setup the duck
+            newDuck.mDuckManager = this;
+            allDucks.Add(newDuck);
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+}
