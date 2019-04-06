a1c79d0afe5a0cc3ea1683ad2fc155e099d776f1
kamilam1987 <kamilam1987@gmail.com>
2019-04-06 20:26:43 +0100

    Unity Speech recognition works

---
+++ KinectGame/Assets/Scripts/Menu/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pausemenuUI;
+
+
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
---
+++ KinectGame/Assets/Scripts/Voice/VoiceTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+
+public class VoiceTest : MonoBehaviour
+{
+    // Waits for string
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    public static bool GameIsPaused = false;
+    public GameObject pausemenuUI;
+
+    void Start()
+    {
+        actions.Add("stop", Pause);
+        actions.Add("pause", Pause);
+        actions.Add("stop game", Pause);
+        actions.Add("pause game", Pause);
+
+        actions.Add("play", Resume);
+        actions.Add("play again", Resume);
+        actions.Add("back", Resume);
+        actions.Add("go back", Resume);
+        actions.Add("resume", Resume);
+
+
+        //checks Array of strings
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    //Recognize what was said
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Pause() {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    private void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
