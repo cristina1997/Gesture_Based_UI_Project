38ff72f28362c86f37da7e3ce302d39c16fb2bc2
unknown <stoyanrizov6@gmail.com>
2019-04-12 09:24:11 +0100

    added bonuses

---
+++ KinectGame/Assets/Scripts/Bonuses/Bonuses.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonuses : MonoBehaviour
+{
+    public Transform[] SpawnPoints;
+    public float spawnTime = .5f;
+    public GameObject[] Coins;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnCoins", spawnTime, spawnTime);
+    }
+
+    void SpawnCoins()
+    {
+        Transform currentSpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform;
+        Instantiate(Coins[Random.Range(0, Coins.Length)], currentSpawnPoint.position, currentSpawnPoint.rotation);
+
+    }
+}
---
+++ KinectGame/Assets/Scripts/Bonuses/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public float destoryTime = .5f;
+    private float rotateSpeed = 300.0f;
+
+    void Start()
+    {
+        Destroy(gameObject, destoryTime);
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+    }
+}
